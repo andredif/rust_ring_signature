aa07142a25786eb9052a24af115b4f9aaea852f5
andredif <difranco.developer@gmail.com>
2023-05-19 11:50:09 +0200

    Correct package version

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
-extern crate clsag;
 extern crate curve25519_dalek;
 use curve25519_dalek::ristretto::{CompressedRistretto, RistrettoPoint};
 use std::time::{Duration, Instant};
-use clsag::member::{Member, generate_signer};
-use clsag::clsag::Clsag;
-use clsag::signature::Signature;
-use clsag::{tests_helper::*, signature};
-use clsag::validation;
+use rust_ring_signature::member::{Member, generate_signer};
+use rust_ring_signature::clsag::Clsag;
+use rust_ring_signature::signature::Signature;
+use rust_ring_signature::{tests_helper::*, signature};
+use rust_ring_signature::validation;
 
 fn main() {
     // Define setup parameters
